72f952b2718f3a374988a66ed984f3a0c1dd5ea1
Gabriel Knupfer <knupfergabriel@gmail.com>
2018-01-04 22:12:42 -0200

    Implementando ambiente de desenvolvimento

---
+++ src/Web Apps/Detetive.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Detetive.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
